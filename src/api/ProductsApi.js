f323fab3441f3b4c538ef924c7c3e84c355df986
XetPy1030 <xetpy1030@mail.ru>
2024-04-11 02:17:55 +0300

    Фикс ProductsApi на правильный вариант

--- src/api/ProductsApi.js
+++ src/api/ProductsApi.js
@@ -1,44 +1,41 @@
 import ApiClass from "@/api/base/BaseApi.js";
 
 class ProductsApi extends ApiClass {
-  static async manufacturerProducts() {
-    const url = this.buildUrl("app/manufacturer/products/");
-    return await this.sendGet({ url });
-  }
+  static INTERMEDIARY_URL = "app/customer/";
 
   static async customerProducts() {
-    const url = this.buildUrl("app/customer/products/");
+    const url = this.buildUrl("products/");
     return await this.sendGet({ url, isAuth: true });
   }
 
   static async customerProduct(productId) {
-    const url = this.buildUrl(`app/customer/products/${productId}/`);
+    const url = this.buildUrl(`products/${productId}/`);
     return await this.sendGet({ url });
   }
 
   static async customerProductCount(data) {
-    const url = this.buildUrl(`app/customer/cart/${data.product_id}/`);
+    const url = this.buildUrl(`cart/${data.product_id}/`);
     return await this.sendPut({ url, data, isAuth: true });
   }
 
   static async getCart() {
-    const url = this.buildUrl("app/customer/cart/");
+    const url = this.buildUrl("cart/");
     return await this.sendGet({ url, isAuth: true });
   }
 
   static async addToCart(data) {
-    const url = this.buildUrl(`app/customer/cart/${data.product_id}/`);
+    const url = this.buildUrl(`cart/${data.product_id}/`);
     return await this.sendPost({ url, data, isAuth: true });
   }
 
   static async removerFromCart(productId) {
-    const url = this.buildUrl(`app/customer/cart/${productId}/`);
+    const url = this.buildUrl(`cart/${productId}/`);
     return await this.sendDelete({ url, isAuth: true });
   }
 
   static async createOrder() {
     // ToDo: доделать создание заказа со всеми путями и т.д., скорее всего нужно будет несколько запросов и переименовать этот
-    // const url = this.buildUrl(`app/customer/cart/${productId}/`);
+    // const url = this.buildUrl(`cart/${productId}/`);
     // return await this.sendDelete({ url, isAuth: true });
   }
 }
