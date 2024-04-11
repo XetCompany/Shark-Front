abbf3a5150e61f290eb68c79ded4fafe6327a3c2
XetPy1030 <xetpy1030@mail.ru>
2024-04-11 20:05:56 +0300

    Более красивая модалка

--- src/pages/Manufacturer/Points/WarehouseProducts.jsx
+++ src/pages/Manufacturer/Points/WarehouseProducts.jsx
@@ -143,7 +143,6 @@ const WarehouseProductsContent = observer(
               transform: "translate(-50%, -50%)",
               width: 400,
               bgcolor: "background.paper",
-              border: "2px solid #000",
               boxShadow: 24,
               p: 4,
             }}
