30fa07c109627b80b4e4a542e00e3abe4be35bdb
XetPy1030 <xetpy1030@mail.ru>
2024-04-12 19:30:29 +0300

    Рейтинг для карточки продукта

--- src/components/Manufacturer/ProductCard.jsx
+++ src/components/Manufacturer/ProductCard.jsx
@@ -1,10 +1,11 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Rating, Typography } from "@mui/material";
 import { useRouterStore } from "mobx-state-router";
 import { RoutesEnum } from "@/router/index.jsx";
 import { MEDIA_URL } from "@/api/constants.js";
 import logo from "@assets/img/no_image.png";
 import { manufacturerStore } from "@store/ManufacturerStore.js";
 import MProductsApi from "@/api/Manufacturer/MProductsApi.js";
+import React from "react";
 
 export function ProductCard({ product, editProductId, setEditProductId }) {
   const routerStore = useRouterStore();
@@ -26,7 +27,7 @@ export function ProductCard({ product, editProductId, setEditProductId }) {
         <Typography variant="h5" component="div">
           {product.name}
         </Typography>
-        <Typography variant="body2">{product.price} руб.</Typography>
+        <Rating name="read-only" value={product.average_rating} precision={0.1} readOnly />
       </CardContent>
       <CardActions
         sx={{
